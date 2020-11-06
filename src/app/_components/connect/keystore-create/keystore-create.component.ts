080768b7a2d102f7da33fe6cb4b0ce17a5d2cb1e
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2020-11-06 19:23:37 +0900

    autologin on create; miniaddress in keystore filename; closes #36; closes #35

--- src/app/_components/connect/keystore-create/keystore-create.component.ts
+++ src/app/_components/connect/keystore-create/keystore-create.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { generatePhrase, encryptToKeyStore } from '@xchainjs/xchain-crypto';
+import { KeystoreService } from 'src/app/_services/keystore.service';
+import { UserService } from 'src/app/_services/user.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-keystore-create',
@@ -15,7 +18,7 @@ export class KeystoreCreateComponent implements OnInit {
   loading: boolean;
   error: boolean;
 
-  constructor() {
+  constructor(private userService: UserService, private keystoreService: KeystoreService) {
     this.loading = false;
     this.phrase = generatePhrase();
     this.back = new EventEmitter<null>();
@@ -26,21 +29,36 @@ export class KeystoreCreateComponent implements OnInit {
   }
 
   async createKeystore() {
-    const keystore = await encryptToKeyStore(this.phrase, this.password);
 
-    // let data = "Whatever it is you want to save";
-    const bl = new Blob([JSON.stringify(keystore)], {
-       type: 'text/plain'
-    });
-    const a = document.createElement('a');
-    a.href = URL.createObjectURL(bl);
-    a.download = 'asgardex-keystore';
-    a.hidden = true;
-    document.body.appendChild(a);
-    a.innerHTML =
-       'loading';
-    a.click();
-    this.back.emit();
+    try {
+      const keystore = await encryptToKeyStore(this.phrase, this.password);
+
+      localStorage.setItem('keystore', JSON.stringify(keystore));
+      const user = await this.keystoreService.unlockKeystore(keystore, this.password);
+      this.userService.setUser(user);
+
+      const binanceAddress = await user.clients.binance.getAddress();
+      const addressLength = binanceAddress.length;
+      const minAddress = `${binanceAddress.substring(0, environment.network === 'testnet' ? 7 : 6)}_${binanceAddress.substring(addressLength - 3, addressLength)}`;
+      const bl = new Blob([JSON.stringify(keystore)], {
+        type: 'text/plain'
+      });
+      const a = document.createElement('a');
+      a.href = URL.createObjectURL(bl);
+      a.download = `asgardex-${minAddress}`;
+      a.hidden = true;
+      document.body.appendChild(a);
+      a.innerHTML =
+        'loading';
+      a.click();
+
+      this.closeModal.next();
+    } catch (error) {
+      console.error(error);
+    }
+
+
+    // this.back.emit();
   }
 
   // back() {
