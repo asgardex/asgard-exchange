7f501107e8f5bcc3c45ccd1c9afbf854c3bc93b4
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2020-11-12 11:39:14 +0900

    copy working on safari; closes #49

--- src/app/_services/copy.service.ts
+++ src/app/_services/copy.service.ts
@@ -8,14 +8,47 @@ export class CopyService {
 
   constructor(private _snackBar: MatSnackBar) { }
 
-  copyToClipboard(text: string) {
-    const listener = (ev) => {
-      ev.preventDefault();
-      ev.clipboardData.setData('text/plain', text);
-    };
-    document.addEventListener('copy', listener);
-    document.execCommand('copy');
-    document.removeEventListener('copy', listener);
+  // pulled from https://stackoverflow.com/a/58276867/3703043
+  copyToClipboard(textToCopy) {
+    let textArea;
+
+    function isOS() {
+      // can use a better detection logic here
+      return navigator.userAgent.match(/ipad|iphone/i);
+    }
+
+    function createTextArea(text) {
+      textArea = document.createElement('textArea');
+      textArea.readOnly = true;
+      textArea.contentEditable = true;
+      textArea.value = text;
+      document.body.appendChild(textArea);
+    }
+
+    function selectText() {
+      let range;
+      let selection;
+
+      if (isOS()) {
+        range = document.createRange();
+        range.selectNodeContents(textArea);
+        selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+        textArea.setSelectionRange(0, 999999);
+      } else {
+        textArea.select();
+      }
+    }
+
+    function copyTo() {
+      document.execCommand('copy');
+      document.body.removeChild(textArea);
+    }
+
+    createTextArea(textToCopy);
+    selectText();
+    copyTo();
 
     this._snackBar.open('Copied to Clipboard', '', {
       duration: 2000,
