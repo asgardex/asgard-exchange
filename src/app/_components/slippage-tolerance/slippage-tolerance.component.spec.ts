e3574eabb27c5982bb5d481be5a778eb1066a2e0
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2020-11-18 19:56:38 +0900

    adds slippage tolerance

---
+++ src/app/_components/slippage-tolerance/slippage-tolerance.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SlippageToleranceComponent } from './slippage-tolerance.component';
+
+describe('SlippageToleranceComponent', () => {
+  let component: SlippageToleranceComponent;
+  let fixture: ComponentFixture<SlippageToleranceComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SlippageToleranceComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SlippageToleranceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
