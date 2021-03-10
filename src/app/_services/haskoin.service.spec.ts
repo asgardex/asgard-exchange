e2ba9837fc837cb52c8dcb96d2309c7b89772867
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2021-03-10 17:25:21 +0900

    pass haskoin service tests

--- src/app/_services/haskoin.service.spec.ts
+++ src/app/_services/haskoin.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HaskoinService } from './haskoin.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('HaskoinService', () => {
   let service: HaskoinService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
     service = TestBed.inject(HaskoinService);
   });
 
