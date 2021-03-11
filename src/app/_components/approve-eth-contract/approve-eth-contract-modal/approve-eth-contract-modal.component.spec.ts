c59e63080599aa18987c87093274549f5b6a1fc1
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2021-03-11 18:18:39 +0900

    pass modal tests

--- src/app/_components/approve-eth-contract/approve-eth-contract-modal/approve-eth-contract-modal.component.spec.ts
+++ src/app/_components/approve-eth-contract/approve-eth-contract-modal/approve-eth-contract-modal.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Asset } from 'src/app/_classes/asset';
 import { ApproveEthContractModalComponent } from './approve-eth-contract-modal.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ApproveEthContractModalComponent', () => {
   let component: ApproveEthContractModalComponent;
@@ -8,7 +10,16 @@ describe('ApproveEthContractModalComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ApproveEthContractModalComponent ]
+      declarations: [ ApproveEthContractModalComponent ],
+      imports: [HttpClientTestingModule],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {
+          contractAddress: '0x9d496De78837f5a2bA64Cb40E62c19FBcB67f55a',
+          asset: new Asset('ETH.DAI-0XAD6D458402F60FD3BD25163575031ACDCE07538D')
+          }
+        },
+        { provide: MatDialogRef, useValue: { close: (dialogResult: any) => { } } }
+      ],
     })
     .compileComponents();
   });
