f3b0a4a57975bacf2fc61adaa8097cce47488320
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2021-03-13 14:42:37 +0900

    adds reusable modal section header

---
+++ src/app/_components/modal-section-header/modal-section-header.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModalSectionHeaderComponent } from './modal-section-header.component';
+import { MatIconModule } from '@angular/material/icon';
+
+
+
+@NgModule({
+  declarations: [ModalSectionHeaderComponent],
+  imports: [
+    CommonModule,
+    MatIconModule
+  ],
+  exports: [ ModalSectionHeaderComponent ]
+})
+export class ModalSectionHeaderModule { }
