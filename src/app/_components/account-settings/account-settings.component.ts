3fcff9bfb9e98699ef0577e55b975ac4e3792eae
HooriRn <rhoormazd@yahoo.com>
2021-03-15 22:39:32 +0330

    account settings update for value

--- src/app/_components/account-settings/account-settings.component.ts
+++ src/app/_components/account-settings/account-settings.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { MainViewsEnum, OverlaysService } from 'src/app/_services/overlays.service';
+import { SlippageToleranceService } from 'src/app/_services/slippage-tolerance.service';
 
 @Component({
   selector: 'app-account-settings',
@@ -10,9 +12,20 @@ export class AccountSettingsComponent implements OnInit {
 
   loading: boolean;
   mode: 'PHRASE' | 'ACCOUNT' | 'SLIP';
+  slippageTolerance: number;
+  subs: Subscription[];
 
-  constructor(private overlaysService: OverlaysService) {
+  constructor(
+    private overlaysService: OverlaysService,
+    private slipLimitService: SlippageToleranceService) {
+
     this.mode = 'ACCOUNT';
+
+    const slippageTolerange$ = this.slipLimitService.slippageTolerance$.subscribe(
+      (limit) => this.slippageTolerance = limit
+    );
+
+    this.subs = [slippageTolerange$]
   }
 
   ngOnInit(): void {
@@ -21,6 +34,12 @@ export class AccountSettingsComponent implements OnInit {
 
   close() {
     this.overlaysService.setViews(MainViewsEnum.Swap, 'Swap');
+  }
+
+  ngOnDestroy() {
+    for (const sub of this.subs) {
+      sub.unsubscribe();
+    }
   }
 
 }
