1628c8187875b0381a8a6452b7b5126632b4a2b8
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2021-03-17 17:55:05 +0900

    :shower: cleanup

--- src/app/swap/swap.component.ts
+++ src/app/swap/swap.component.ts
@@ -527,9 +527,6 @@ export class SwapComponent implements OnInit, OnDestroy {
     const sourcePool = this.poolDetailMap[`${this.selectedSourceAsset.chain}.${this.selectedSourceAsset.symbol}`];
     const targetPool = this.poolDetailMap[`${this.selectedTargetAsset.chain}.${this.selectedTargetAsset.symbol}`];
 
-    console.log('source pool is: ', sourcePool);
-    console.log('target pool is: ', targetPool);
-
     if (sourcePool && targetPool) {
       const pool1: PoolData = {
         assetBalance: baseAmount(sourcePool.assetDepth),
