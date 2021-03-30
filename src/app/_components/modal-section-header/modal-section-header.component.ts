ae8f6063c147f3d1aee42d6870d0839c325d675e
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2021-03-30 17:18:37 +0900

    ui work to standardize close modal btn

--- src/app/_components/modal-section-header/modal-section-header.component.ts
+++ src/app/_components/modal-section-header/modal-section-header.component.ts
@@ -10,11 +10,15 @@ export class ModalSectionHeaderComponent implements OnInit {
   @Input() sectionTitle?: string | null;
   @Input() sectionTitleIcon?: string | null;
   @Input() displayBackBtn: boolean;
+  @Input() displayCloseBtn: boolean;
   @Output() back: EventEmitter<null>;
+  @Output() close: EventEmitter<null>;
 
   constructor() {
     this.displayBackBtn = false;
+    this.displayCloseBtn = false;
     this.back = new EventEmitter<null>();
+    this.close = new EventEmitter<null>();
   }
 
   ngOnInit(): void {
