396c0e909fda353bc470a41c35d50f7570b5754d
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2021-04-12 22:52:23 +0900

    pass linting

--- src/app/swap/confirm-swap-modal/confirm-swap-modal.component.ts
+++ src/app/swap/confirm-swap-modal/confirm-swap-modal.component.ts
@@ -93,7 +93,10 @@ export class ConfirmSwapModalComponent implements OnInit, OnDestroy {
 
   async estimateTime() {
     if (this.swapData.sourceAsset.chain === 'ETH' && this.swapData.sourceAsset.symbol !== 'ETH') {
-      this.estimatedMinutes = await this.ethUtilsService.estimateERC20Time(assetToString(this.swapData.sourceAsset), this.swapData.inputValue);
+      this.estimatedMinutes = await this.ethUtilsService.estimateERC20Time(
+        assetToString(this.swapData.sourceAsset),
+        this.swapData.inputValue
+      );
     } else {
       this.estimatedMinutes = this.txStatusService.estimateTime(this.swapData.sourceAsset.chain, this.swapData.inputValue);
     }
