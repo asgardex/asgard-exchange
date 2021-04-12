77a3db4ee0b8eae7d11fc1851b6e610e0e5b8b67
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2021-04-12 19:25:56 +0900

    pass linting

--- src/app/_classes/network.ts
+++ src/app/_classes/network.ts
@@ -1,23 +1,23 @@
 export interface NetworkSummary {
   activeBonds: string[];
   activeNodeCount: string;
-	blockRewards: {
-		blockReward: string,
-		bondReward: string,
-		poolReward: string
-	},
+  blockRewards: {
+    blockReward: string,
+    bondReward: string,
+    poolReward: string
+  };
   bondMetrics: {
-		averageActiveBond: string,
-		averageStandbyBond: string,
-		maximumActiveBond: string,
-		maximumStandbyBond: string,
-		medianActiveBond: string,
-		medianStandbyBond: string,
-		minimumActiveBond: string,
-		minimumStandbyBond: string,
-		totalActiveBond: string,
-		totalStandbyBond: string
-	},
+    averageActiveBond: string,
+    averageStandbyBond: string,
+    maximumActiveBond: string,
+    maximumStandbyBond: string,
+    medianActiveBond: string,
+    medianStandbyBond: string,
+    minimumActiveBond: string,
+    minimumStandbyBond: string,
+    totalActiveBond: string,
+    totalStandbyBond: string
+  };
   bondingAPY: string;
   liquidityAPY: string;
   nextChurnHeight: string;
