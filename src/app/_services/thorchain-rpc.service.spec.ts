3aa86c8498f7d6c16a05a66de0533b0ec6d8d462
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2021-04-25 16:44:13 +0900

    poll rpc endpoint for THOR transfers

---
+++ src/app/_services/thorchain-rpc.service.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ThorchainRpcService } from './thorchain-rpc.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+describe('ThorchainRpcService', () => {
+  let service: ThorchainRpcService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    service = TestBed.inject(ThorchainRpcService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
---
+++ src/app/_services/thorchain-rpc.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+export interface RpcTxSearchRes {
+  result: {
+    total_count: string;
+    txs: {
+      hash: string;
+      height: string;
+    }[];
+  };
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThorchainRpcService {
+
+  private _basePath: string;
+
+  constructor(private http: HttpClient) {
+    this._basePath = (environment.network === 'testnet')
+      ? 'https://testnet.rpc.thorchain.info'
+      : 'https://rpc.thorchain.info';
+  }
+
+  txSearch(sender: string): Observable<RpcTxSearchRes> {
+    const params = new HttpParams().set('query', `"transfer.sender='${sender}'"`).set('per_page', '"100"').set('order_by', '"desc"');
+    return this.http.get<RpcTxSearchRes>(`${this._basePath}/tx_search`, {params});
+  }
+
+}
