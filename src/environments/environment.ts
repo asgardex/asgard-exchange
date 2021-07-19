02fa4d535788194320821709649e57a3ced2436f
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2021-07-19 17:06:42 +0900

    pool create reverse asset

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
    production: false,
    network: 'testnet',
-   etherscanKey: 'XIPF6BTF5JANIQCBZC675R6BWAF2B7G6XY',
-   infuraProjectId: 'c9cd2bfd3d164a12b70e4479167d4e22',
+   etherscanKey: '',
+   infuraProjectId: '',
    appLocked: false
 };
